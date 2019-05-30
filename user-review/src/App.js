7240bd3fda24daf4adf41a514925ff6807ff5a36
VirtualRcoder <shubhamsh2820@gmail.com>
2019-05-30 13:02:22 +0530

    Added slideshow on home

--- user-review/src/App.js
+++ user-review/src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import Slideshow from "./components/Slideshow/Slideshow";
 import Body from "./components/Body/Body";
 import Form from "./components/Form/Login Form";
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <Navbar /><br/><br/><br/><br/><br/><br/><br/>
-        <Body/>
+        <Navbar /><br/><br/><br/>
+        <Slideshow />
+       	<Body />
       </React.Fragment>
     );
   }
